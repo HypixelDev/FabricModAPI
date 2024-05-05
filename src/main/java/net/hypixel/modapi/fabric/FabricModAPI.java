6273ec65620129375ddf6ca90d98a8252f3ee7c7
Connor Linfoot <connorlinfoot@me.com>
2024-05-05 14:30:00 +0100

    Add method to reload registrations

--- src/main/java/net/hypixel/modapi/fabric/FabricModAPI.java
+++ src/main/java/net/hypixel/modapi/fabric/FabricModAPI.java
@@ -11,7 +11,6 @@ import net.hypixel.modapi.fabric.payload.ServerboundHypixelPayload;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.packet.CustomPayload;
-import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 
 public class FabricModAPI implements ClientModInitializer {
@@ -19,57 +18,77 @@ public class FabricModAPI implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        registerPayloads();
+        reloadRegistrations();
         registerPacketSender();
     }
 
-    private void registerPayloads() {
-        for (String identifier : HypixelModAPI.getInstance().getRegistry().getIdentifiers()) {
-            registerClientbound(identifier);
-            registerServerbound(identifier);
+    /**
+     * Reloads the identifiers that are registered in the Hypixel Mod API and makes sure that the packets are registered.
+     * <p>
+     * This method is available for internal use by Hypixel to add new packets externally, and is not intended for use by other developers.
+     */
+    public static void reloadRegistrations() {
+        for (String identifier : HypixelModAPI.getInstance().getRegistry().getClientboundIdentifiers()) {
+            try {
+                registerClientbound(identifier);
+            } catch (Exception e) {
+                LOGGER.error("Failed to register clientbound packet with identifier '{}'", identifier, e);
+            }
+        }
+
+        for (String identifier : HypixelModAPI.getInstance().getRegistry().getServerboundIdentifiers()) {
+            try {
+                registerServerbound(identifier);
+            } catch (Exception e) {
+                LOGGER.error("Failed to register serverbound packet with identifier '{}'", identifier, e);
+            }
         }
     }
 
-    private void registerPacketSender() {
+    private static void registerPacketSender() {
         HypixelModAPI.getInstance().setPacketSender((packet) -> {
             ServerboundHypixelPayload payload = new ServerboundHypixelPayload(packet);
             ClientPlayNetworking.send(payload);
         });
     }
 
-    public void registerClientbound(String identifier) {
-        Validate.isTrue(HypixelModAPI.getInstance().getRegistry().isRegistered(identifier), "Identifier %s is not registered", identifier);
+    private static void registerClientbound(String identifier) {
+        try {
+            CustomPayload.Id<ClientboundHypixelPayload> clientboundId = CustomPayload.id(identifier);
+            PacketCodec<PacketByteBuf, ClientboundHypixelPayload> codec = ClientboundHypixelPayload.buildCodec(clientboundId);
+            PayloadTypeRegistry.playS2C().register(clientboundId, codec);
 
-        CustomPayload.Id<ClientboundHypixelPayload> clientboundId = CustomPayload.id(identifier);
-        PacketCodec<PacketByteBuf, ClientboundHypixelPayload> codec = ClientboundHypixelPayload.buildCodec(clientboundId);
-        PayloadTypeRegistry.playS2C().register(clientboundId, codec);
+            // Also register the global receiver for handling incoming packets
+            ClientPlayNetworking.registerGlobalReceiver(clientboundId, (payload, context) -> {
+                if (!payload.isSuccess()) {
+                    LOGGER.warn("Received an error response for packet {}: {}", identifier, payload.getErrorReason());
+                    return;
+                }
 
-        // Also register the global receiver for handling incoming packets
-        ClientPlayNetworking.registerGlobalReceiver(clientboundId, (payload, context) -> {
-            if (!payload.isSuccess()) {
-                LOGGER.warn("Received an error response for packet {}: {}", identifier, payload.getErrorReason());
-                return;
-            }
+                try {
+                    HypixelModAPI.getInstance().handle(payload.getPacket());
+                } catch (Exception e) {
+                    LOGGER.error("An error occurred while handling packet {}", identifier, e);
+                }
 
-            try {
-                HypixelModAPI.getInstance().handle(payload.getPacket());
-            } catch (Exception e) {
-                LOGGER.error("An error occurred while handling packet {}", identifier, e);
-            }
-
-            try {
-                HypixelModAPICallback.EVENT.invoker().onPacketReceived(payload.getPacket());
-            } catch (Exception e) {
-                LOGGER.error("An error occurred while handling packet {}", identifier, e);
-            }
-        });
+                try {
+                    HypixelModAPICallback.EVENT.invoker().onPacketReceived(payload.getPacket());
+                } catch (Exception e) {
+                    LOGGER.error("An error occurred while handling packet {}", identifier, e);
+                }
+            });
+        } catch (IllegalArgumentException ignored) {
+            // Ignored as this is fired when we reload the registrations and the packet is already registered
+        }
     }
 
-    public void registerServerbound(String identifier) {
-        Validate.isTrue(HypixelModAPI.getInstance().getRegistry().isRegistered(identifier), "Identifier %s is not registered", identifier);
-
-        CustomPayload.Id<ServerboundHypixelPayload> serverboundId = CustomPayload.id(identifier);
-        PacketCodec<PacketByteBuf, ServerboundHypixelPayload> codec = ServerboundHypixelPayload.buildCodec(serverboundId);
-        PayloadTypeRegistry.playC2S().register(serverboundId, codec);
+    private static void registerServerbound(String identifier) {
+        try {
+            CustomPayload.Id<ServerboundHypixelPayload> serverboundId = CustomPayload.id(identifier);
+            PacketCodec<PacketByteBuf, ServerboundHypixelPayload> codec = ServerboundHypixelPayload.buildCodec(serverboundId);
+            PayloadTypeRegistry.playC2S().register(serverboundId, codec);
+        } catch (IllegalArgumentException ignored) {
+            // Ignored as this is fired when we reload the registrations and the packet is already registered
+        }
     }
 }
